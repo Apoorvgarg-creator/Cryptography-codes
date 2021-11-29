6ddb476cb7f184c72e14db1b5e925574ffc3fad0
Apoorvgarg-creator <APOORVGARG.21@GMAIL.COM>
2021-11-29 16:07:18 +0530

    rail fence code corrected

--- Rail_fence.cpp
+++ Rail_fence.cpp
@@ -1,8 +1,11 @@
 #include<stdio.h>
+#include<iostream>
 #include<string.h>
  
-void encryptMsg(char msg[], int key){
-    int msgLen = strlen(msg), i, j, k = -1, row = 0, col = 0;
+using namespace std;
+
+string encryptMsg(string msg, int key){
+    int msgLen = msg.length(), i, j, k = -1, row = 0, col = 0;
     char railMatrix[key][msgLen];
  
     for(i = 0; i < key; ++i)
@@ -19,15 +22,18 @@ void encryptMsg(char msg[], int key){
     }
  
     printf("\nEncrypted Message: ");
- 
+    string enMsg = "";
     for(i = 0; i < key; ++i)
         for(j = 0; j < msgLen; ++j)
-            if(railMatrix[i][j] != '\n')
-                printf("%c", railMatrix[i][j]);
+            if(railMatrix[i][j] != '\n') {
+                cout<<railMatrix[i][j];
+                enMsg += railMatrix[i][j];
+            }
+    return enMsg;
 }
  
-void decryptMsg(char enMsg[], int key){
-    int msgLen = strlen(enMsg), i, j, k = -1, row = 0, col = 0, m = 0;
+void decryptMsg(string enMsg, int key){
+    int msgLen = enMsg.length(), i, j, k = -1, row = 0, col = 0, m = 0;
     char railMatrix[key][msgLen];
  
     for(i = 0; i < key; ++i)
@@ -54,8 +60,7 @@ void decryptMsg(char enMsg[], int key){
     printf("\nDecrypted Message: ");
  
     for(i = 0; i < msgLen; ++i){
-        printf("%c", railMatrix[row][col++]);
- 
+        cout<<railMatrix[row][col++];
         if(row == 0 || row == key-1)
             k= k * (-1);
  
@@ -64,15 +69,16 @@ void decryptMsg(char enMsg[], int key){
 }
  
 int main(){
-    char msg[] = "Apoorv Garg";
-    char enMsg[] = "graG vroopA";
+    string msg, enMsg;
     int key = 3;
+    cout<<"Enter Key: ";
+    cin>>key;
+    cout<<"Enter message: ";
+    cin>>msg;
  
-    printf("Original Message: %s", msg);
- 
-    encryptMsg(msg, key);
+    cout<<"Original Message: "<<msg<<"\n";
+    enMsg = encryptMsg(msg, key);
     decryptMsg(enMsg, key);
  
     return 0;
 }
-
